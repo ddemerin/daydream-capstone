c5d514dc1fb8e19050987f84ba5c58241c289ea8
Daniel D Demerin <ddemerin@gmail.com>
2020-04-15 14:12:27 -0400

    Adjusted front page styling

--- ClientApp/src/components/Featured.jsx
+++ ClientApp/src/components/Featured.jsx
@@ -8,10 +8,12 @@ const Featured = props => {
   return (
     <>
       <div className="featured-book-container">
-        <div className="featured-book-border">
-          <a href={`/read/${book.id}`} className="featured-book-link">
-            <img src={book.pages[0].imageUrl} className="featured-book" />
-          </a>
+        <div className="featured-book-box">
+          <div className="featured-book-border">
+            <a href={`/read/${book.id}`} className="featured-book-link">
+              <img src={book.pages[0].imageUrl} className="featured-book" />
+            </a>
+          </div>
         </div>
       </div>
       <div className="featured-book-writeup-container">
